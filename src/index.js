fea8d51d63d79f5558246af2b6b5928f3e804d9a
FeelingHxllxw <malchenko.03@mail.ru>
2024-05-10 17:27:27 +0500

    new file:   src/assets/img/minus.png new file:   src/assets/img/pepperoni.png new file:   src/assets/img/plus.png new file:   "src/assets/img/\320\234\320\276\321\206\320\260\321\200\320\265\320\273\320\273\320\260.jpg" new file:   "src/assets/img/\320\261\320\260\320\262\320\260\321\200\321\201\320\272\320\260\321\217.png" new file:   "src/assets/img/\320\274\320\260\321\200\320\263\320\260\321\200\320\270\321\202\320\260.png" new file:   "src/assets/img/\321\201\321\213\321\200\320\275\321\213\320\271 \320\261\320\276\321\200\321\202.png" new file:   "src/assets/img/\321\207\320\265\320\264\320\264\320\265\321\200.jpg" modified:   src/assets/styles/style.css modified:   src/index.html

--- src/index.js
+++ src/index.js
@@ -1,95 +1,129 @@
+pizzaTypes = {
+  Маргарита: [500, 300],
+  Пепперони: [800, 400],
+  Баварская: [700, 450],
+};
+sizes = {
+  Большая: [200, 200],
+  Маленькая: [100, 100],
+};
+toppings = {
+  "Сырный борт": [150, 50],
+  "Сливочная моцарелла": [50, 20],
+  "Чеддр и пармезан": [150, 50],
+};
+
 class Pizza {
-    constructor(name, price, calories) {
-      this.name = name;
-      this.price = price;
-      this.calories = calories;
-      this.toppings = [];
-      this.size = null;
-    }
-  
-    addTopping(topping) {
-      this.toppings.push(topping);
-    }
-  
-    removeTopping(topping) {
-      this.toppings = this.toppings.filter((t) => t !== topping);
-    }
-  
-    getSize() {
-      return this.size;
-    }
-  
-    setSize(size) {
-      this.size = size;
-      this.price += size.price;
-      this.calories += size.calories;
-    }
-  
-    calculatePrice() {
-      return this.price + this.toppings.reduce((total, t) => total + t.price, 0);
-    }
-  
-    calculateCalories() {
-      return (
-        this.calories +
-        this.toppings.reduce((total, t) => total + t.calories, 0)
-      );
-    }
+  constructor(name, size, topping, price, calorie) {
+    this.name = name;
+    this.size = size;
+    this.topping = [];
+    this.price = 0;
+    this.calorie = 0;
   }
-  
-  class PizzaSize {
-    constructor(name, price, calories) {
-      this.name = name;
-      this.price = price;
-      this.calories = calories;
-    }
+  getPrice() {
+    return this.price;
   }
-  
-  class Topping {
-    constructor(name, price, calories) {
-      this.name = name;
-      this.price = price;
-      this.calories = calories;
-    }
+  getCalorie() {
+    return this.calorie;
   }
-  
+  addTopping(newTopping) {
+    this.topping.push(newTopping);
+    this.calcCalorie();
+    this.calcPrice();
+  }
+  removeTopping(oldTopping) {
+    this.calcCalorie();
+    this.calcPrice();
+    let toppingPrice = oldTopping[0];
+    let toppingCalorie = oldTopping[1];
+    this.size == "Большая"
+      ? (this.price -= toppingPrice * 2)
+      : (this.price -= toppingPrice);
+    this.calorie -= toppingCalorie;
+    let index = this.topping.findIndex((top) => top === oldTopping);
+    this.topping.splice(index, 1);
+    this.calcCalorie();
+    this.calcPrice();
+  }
 
-  
-  const margherita = new Pizza("Маргарита", 500, 300);
-  const pepperoni = new Pizza("Пепперони", 800, 400);
-  const bavarian = new Pizza("Баварская", 700, 450);
-  
-  const largeSize = new PizzaSize("Большая", 200, 200);
-  const smallSize = new PizzaSize("Маленькая", 100, 100);
-  
-  const mozzarella = new Topping("Cливочная моцарелла", 50, 20);
-  const cheeseEdgeSmall = new Topping("Cырный борт для маленькой", 150, 50);
-  const cheeseEdgeLarge = new Topping("Сырный борт для большой", 300, 50);
-  const cheddarParmesanSmall = new Topping("Чедер и пармезан для маленькой", 150, 50);
-  const cheddarParmesanLarge = new Topping("Чедер и пармезан для большой", 300, 50);
-  
+  calcPrice() {
+    this.price = 0;
+    this.price += pizzaTypes[this.name][0];
+    this.price += sizes[this.size][0];
+    let size = this.size;
 
-  
-  margherita.setSize(largeSize);
-  margherita.addTopping(mozzarella);
-  margherita.addTopping(cheeseEdgeLarge);
-  
-  pepperoni.setSize(smallSize);
-  pepperoni.addTopping(cheeseEdgeSmall);
-  pepperoni.addTopping(cheddarParmesanSmall);
-  
-  bavarian.setSize(largeSize);
-  bavarian.addTopping(mozzarella);
-  bavarian.addTopping(cheeseEdgeLarge);
-  bavarian.addTopping(cheddarParmesanLarge);
-  
+    this.topping.forEach((item) => {
+      size == "Большая"
+        ? (this.price += toppings[item][0] * 2)
+        : (this.price += toppings[item][0]);
+    });
 
-  
-  console.log(margherita.calculatePrice()); 
-  console.log(margherita.calculateCalories()); 
-  
-  console.log(pepperoni.calculatePrice()); 
-  console.log(pepperoni.calculateCalories()); 
-  
-  console.log(bavarian.calculatePrice());
-  console.log(bavarian.calculateCalories()); 
+    document.getElementById(
+      "add-2-cart"
+    ).textContent = `${customPizza.getPrice()} руб., (${customPizza.getCalorie()} кКал)`;
+  }
+  calcCalorie() {
+    this.calorie = 0;
+    this.calorie += pizzaTypes[this.name][1];
+    this.calorie += sizes[this.size][1];
+    let size = this.size;
+
+    this.topping.forEach((item) => {
+      size == "Большая"
+        ? (this.calorie += toppings[item][1] * 2)
+        : (this.calorie += toppings[item][1]);
+    });
+    document.getElementById(
+      "add-2-cart"
+    ).textContent = `${customPizza.getPrice()} руб., (${customPizza.getCalorie()} кКал)`;
+  }
+  getToppings() {
+    return this.topping;
+  }
+  getSize() {
+    return this.size;
+  }
+  getStuffing() {
+    return this.name;
+  }
+}
+let customPizza;
+function choosePizza(chosenPizza) {
+  document
+    .querySelectorAll(".topping-item")
+    .forEach((item) => (item.style.border = ""));
+  customPizza = chosenPizza;
+  document
+    .querySelectorAll(".pizza-item")
+    .forEach((item) => (item.style.border = ""));
+  document.getElementById(chosenPizza.getStuffing()).style.border =
+    "5px solid orange";
+  customPizza.calcCalorie();
+  customPizza.calcPrice();
+}
+function actTop(toppingOption) {
+  if (!customPizza.topping.includes(toppingOption)) {
+    console.log(customPizza.topping);
+    addTop(toppingOption);
+  } else {
+    remTop(toppingOption);
+  }
+}
+function remTop(toppingName) {
+  customPizza.removeTopping(toppingName);
+  document.getElementById(toppingName).style.border = "0px solid orange";
+}
+function addTop(toppingName) {
+  customPizza.addTopping(toppingName);
+  document.getElementById(toppingName).style.border = "5px solid orange";
+}
+
+function resizePizza() {
+  document.getElementById("radioS").checked == "1"
+    ? (customPizza.size = "Маленькая")
+    : (customPizza.size = "Большая");
+
+  customPizza.calcCalorie();
+  customPizza.calcPrice();
+}
