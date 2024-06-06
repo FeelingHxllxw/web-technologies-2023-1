abd66e8209decb6778a6053756da687ae9c5b3de
FeelingHxllxw <malchenko.03@mail.ru>
2024-06-06 16:30:08 +0500

    lesson19

--- index.php
+++ index.php
@@ -1,91 +1,39 @@
+
 <?php
-//Задание 1
-$a = -10;
-$b = -5;
-
-function calculate($a, $b) {
-    if ($a >= 0 && $b >= 0) {
-        return $a - $b;
-    } elseif ($a < 0 && $b < 0) {
-        return $a * $b;
-    } else {
-        return $a + $b;
+    function getImages() {
+        $dir = __DIR__ . DIRECTORY_SEPARATOR . "Галерея" . DIRECTORY_SEPARATOR;
+        $images = glob(
+            "$dir*.{png,jpg,jpeg}",
+            GLOB_BRACE
+        );
+        
+        foreach($images as $image) {
+            printf("<a href='Галерея/%s' target='_blank'><img src='Галерея/%s' height=250></a>",
+    rawurldecode(basename($image)),
+    rawurldecode(basename($image))
+);
+        }
     }
-}
-
-$result1 = calculate($a, $b);
-echo ("<p><b>Задание 1</b></p>" . "<p>Результат = " . $result1 . "</p>");
-
-//Задание 2
-$result2 = "";
-for ($i = $a; $i <= 15; $i++) {
-    $result2 .= $i . ", ";
-}
-$result2 = rtrim($result2, ", ");
-echo ("<p><b>Задание 2</b></p>" . "<p>Результат = " . $result2 . "</p>");
-
-//Задание 3
-function summ($a, $b) {
-    return $a + $b;
-}
-
-function multiplication($a, $b) {
-    return $a * $b;
-}
-
-function difference($a, $b) {
-    return $a - $b;
-}
-
-function division($a, $b) {
-    return $a / $b;
-}
-
-echo ("<p><b>Задание 3</b></p>" .
-"<p>3 + 5 = " . summ(3, 5) . "</p>" .
-"<p>8 - 4 = " . difference(8, 4) . "</p>" .
-"<p>7 * 4 = " . multiplication(7, 4) . "</p>" .
-"<p>9 / 3 = " . division(9, 3) . "</p>");
-
-//Задание 4
-function mathOperation($arg1, $arg2, $operation) {
-    switch ($operation) {
-        case '+':
-            return summ($arg1, $arg2);
-        case '-':
-            return difference($arg1, $arg2);
-        case '*':
-            return multiplication($arg1, $arg2);
-        case '/':
-            return division($arg1, $arg2);
-        default:
-            return 0;
-    }
-}
-echo ("<p><b>Задание 4</b></p>" .
-"<p>Сумма чисел 15 и 7 = " . mathOperation(15, 7, "+") . "</p>");
-
-//Задание 6
-function power($val, $pow) {
-    if ($pow == 0) {
-        return 1;
-    } elseif ($pow < 0) {
-        return 1 / power($val, -$pow);
-    } else {
-        return $val * power($val, $pow - 1);
-    }
-}
-echo ("<p><b>Задание 6</b></p>" .
-"<p>Число 2 в 4 степени = " . power(2, 4) . "</p>");
 ?>
 
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Задание 17</title>
-</head>
-<body>
 
-</body>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Фото галерея</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./styles.css">
+    </head>
+    <body> 
+        <div class="gallery">
+            <?php getImages() ?>
+        </div>
+        <form method="post" action="gallery.php" enctype="multipart/form-data">
+                <input type="file" name="fileToUpload" id="fileToUpload"></input>
+                <input type="submit" name="submit"></input>
+            </form>
+    </body>
 </html>
