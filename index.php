7c4719dd11ebf4ce98a520ef27deae9bf9cd2a65
FeelingHxllxw <malchenko.03@mail.ru>
2024-06-06 16:07:03 +0500

    Лаб 17

--- index.php
+++ index.php
@@ -1,47 +1,91 @@
 <?php
-date_default_timezone_set('Asia/Yekaterinburg');
-$title = 'php-16';
-$content_h1 = "Время";
+//Задание 1
+$a = -10;
+$b = -5;
 
-function getCorrectFormatTime($current_time): string
-{
-    $current_hour = $current_time['tm_hour'];
-    $current_min = $current_time['tm_min'];
-
-    if ($current_hour > 4 && $current_hour < 21) {
-        $hour_ps = 'часов';
-    } elseif ($current_hour % 10 == 1) {
-        $hour_ps = 'час';
+function calculate($a, $b) {
+    if ($a >= 0 && $b >= 0) {
+        return $a - $b;
+    } elseif ($a < 0 && $b < 0) {
+        return $a * $b;
     } else {
-        $hour_ps = 'часа';
+        return $a + $b;
     }
-
-    if (($current_min > 4 && $current_min < 21) || $current_min % 10 == 0 || $current_min % 10 > 4) {
-        $min_ps = 'минут';
-    } elseif ($current_min % 10 == 1) {
-        $min_ps = 'минута';
-    } else {
-        $min_ps = 'минуты';
-    }
-
-    return sprintf("%d %s %d %s", $current_hour, $hour_ps, $current_min, $min_ps);
 }
 
-$current_time = localtime(time(), true);
-$result = getCorrectFormatTime($current_time);
+$result1 = calculate($a, $b);
+echo ("<p><b>Задание 1</b></p>" . "<p>Результат = " . $result1 . "</p>");
+
+//Задание 2
+$result2 = "";
+for ($i = $a; $i <= 15; $i++) {
+    $result2 .= $i . ", ";
+}
+$result2 = rtrim($result2, ", ");
+echo ("<p><b>Задание 2</b></p>" . "<p>Результат = " . $result2 . "</p>");
+
+//Задание 3
+function summ($a, $b) {
+    return $a + $b;
+}
+
+function multiplication($a, $b) {
+    return $a * $b;
+}
+
+function difference($a, $b) {
+    return $a - $b;
+}
+
+function division($a, $b) {
+    return $a / $b;
+}
+
+echo ("<p><b>Задание 3</b></p>" .
+"<p>3 + 5 = " . summ(3, 5) . "</p>" .
+"<p>8 - 4 = " . difference(8, 4) . "</p>" .
+"<p>7 * 4 = " . multiplication(7, 4) . "</p>" .
+"<p>9 / 3 = " . division(9, 3) . "</p>");
+
+//Задание 4
+function mathOperation($arg1, $arg2, $operation) {
+    switch ($operation) {
+        case '+':
+            return summ($arg1, $arg2);
+        case '-':
+            return difference($arg1, $arg2);
+        case '*':
+            return multiplication($arg1, $arg2);
+        case '/':
+            return division($arg1, $arg2);
+        default:
+            return 0;
+    }
+}
+echo ("<p><b>Задание 4</b></p>" .
+"<p>Сумма чисел 15 и 7 = " . mathOperation(15, 7, "+") . "</p>");
+
+//Задание 6
+function power($val, $pow) {
+    if ($pow == 0) {
+        return 1;
+    } elseif ($pow < 0) {
+        return 1 / power($val, -$pow);
+    } else {
+        return $val * power($val, $pow - 1);
+    }
+}
+echo ("<p><b>Задание 6</b></p>" .
+"<p>Число 2 в 4 степени = " . power(2, 4) . "</p>");
 ?>
-<!doctype html>
-<html lang="en">
+
+<!DOCTYPE html>
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title><?= $title ?></title>
+    <title>Задание 17</title>
 </head>
 <body>
-    <h1><?= $content_h1 ?></h1>
-    <h2><?= $result ?></h2>
+
 </body>
 </html>
